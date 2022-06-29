baf97f58327b7db4a2f958a987423b6b436b9547
ummuarmi <59118904+ummuarmi@users.noreply.github.com>
2022-06-29 21:12:57 +0800

    Update iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
@@ -28,7 +27,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
+iris = datasets.load_iris("https://raw.githubusercontent.com/ummuarmi/irisClassification/main/IRIS.csv")
 X = iris.data
 Y = iris.target
 
